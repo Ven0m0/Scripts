6fd360d0965c63cfbf9259f218eccc5bef7ab6a8
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-16 20:05:53 +0200

    Add files via upload

---
+++ Other/Citra-per-game-config/3d Land save.cmd
@@ -0,0 +1,8 @@
+@echo off 
+SET Config=%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config\qt-config.ini
+Set Land=%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra-per-game-config\3d-land\qt-config.ini
+Set Default=%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra-per-game-config\Default\qt-config.ini
+
+Copy %Config% %Land% 
+Copy %Default% %Config%
+exit
---
+++ Other/Citra-per-game-config/No prefetch.cmd
@@ -0,0 +1,6 @@
+@echo off 
+SET Config=%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config\qt-config.ini
+Set Prefetch=%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra-per-game-config\No-prefetch\qt-config.ini
+
+Copy %Prefetch% %Config%
+pause
