c8e070b3b5a1499875378de3b70162519fca74d9
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-16 18:32:42 +0200

    Delete Citra per game config directory

--- Citra per game config/3d Land.cmd
+++
@@ -1,6 +0,0 @@
-@echo off 
-SET ConfigFolder=%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config
-Set Land= "%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\Game configs\3d land\qt-config.ini"
-
-Copy %Land% %ConfigFolder%
-exit
--- Citra per game config/No prefetch.cmd
+++
@@ -1,6 +0,0 @@
-@echo off 
-SET ConfigFolder=%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config
-Set Prefetch= "%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\Game configs\No prefetch\qt-config.ini"
-
-Copy %Prefetch% %ConfigFolder%
-exit
