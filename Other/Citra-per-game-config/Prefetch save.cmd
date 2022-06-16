b031a9c3ed5a7c5cf82b6e608087f1426bce080b
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-16 21:43:20 +0200

    Delete Other/Citra-per-game-config directory

--- Other/Citra-per-game-config/Prefetch save.cmd
+++
@@ -1,8 +0,0 @@
-@echo off 
-SET Config=%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config\qt-config.ini
-Set Prefetch=%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra-per-game-config\No-prefetch\qt-config.ini
-Set Default=%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra-per-game-config\Default\qt-config.ini
-
-Copy %Config% %Prefetch% 
-Copy %Default% %Config%
-pause
