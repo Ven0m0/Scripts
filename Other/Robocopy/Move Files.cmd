23ab9216449ab82d429c641420cb9a79d188ed75
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-16 18:38:54 +0200

    Delete Other/Robocopy directory

--- Other/Robocopy/Move Files.cmd
+++
@@ -1,4 +0,0 @@
-robocopy "%USERPROFILE%\OneDriveBackup\Optimal\Files\Program Files (x86)" "C:" /MT:32 /z /j /s /e
-robocopy "%USERPROFILE%\OneDrive\Backup\Optimal\Files\ProgramData" "C:" /MT:32 /z /j /s /e
-robocopy "%USERPROFILE%\OneDrive\Backup\Optimal\Files\Users" %USERPROFILE% /MT:32 /z /j /s /e
-exit
