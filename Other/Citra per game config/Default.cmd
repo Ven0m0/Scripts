c51357d95c56234842f89ed9b431f38f3e9f1a01
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-17 19:48:21 +0200

    Delete Other/Citra per game config directory

--- Other/Citra per game config/Default.cmd
+++
@@ -1,6 +0,0 @@
-@echo off 
-SET ConfigFolder= "%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config"
-Set Default= "%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Citra per game config\Default\qt-config.ini"
-
-Copy %Default% %ConfigFolder%
-exit
