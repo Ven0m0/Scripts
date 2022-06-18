fe500fd57c9de3c04b3058997ca22b17bab9f131
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-18 02:52:10 +0200

    Delete PlayniteFullscreen.bat

--- Other/Playnite/PlayniteFullscreen.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-START "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --video-on-top --play-and-exit --no-video-title -Idummy "%USERPROFILE%\OneDrive\Backup\Game\Other\Launchers\Playnite\Videos\BootVideo.mp4"
-TIMEOUT /T 3
-START "" "%USERPROFILE%\OneDrive\Backup\Game\Other\Launchers\Playnite\Playnite.FullscreenApp.exe" --hidesplashscreen
-exit
