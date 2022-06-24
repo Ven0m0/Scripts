660705e3b935d4667ec3d0dca35272f41d5d71d8
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2022-06-24 22:19:04 +0200

    Add files via upload

---
+++ Other/Citra per game config/No prefetch.cmd
@@ -0,0 +1,8 @@
+@echo off 
+Set Config= "%USERPROFILE%\OneDrive\Backup\Game\Emul\Citra\canary-mingw\user\config\qt-config.ini"
+Set Prefetch= "%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Other\Playnite\Citra per game config\No prefetch\qt-config.ini"
+Set Default= "%USERPROFILE%\OneDrive\Backup\Optimal\Scripts\Other\Playnite\Citra per game config\Default\qt-config.ini"
+
+Copy %Config% %Default%
+Copy %Prefetch% %Config%
+exit
